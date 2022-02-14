53f1924ba321f5c49dcd0ae8da9e079c531fe0d9
Ingrid <dih_lopes@hotmail.com>
2022-02-13 21:35:52 -0300

    feito alteracoes na rota post/upload porem nao funcionou

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const fileupload = require("express-fileupload");
 var cors = require("cors");
 
 const produtos = require("./rotas/produtos");
@@ -7,10 +8,10 @@ const cor = require("./rotas/cor");
 
 
 const {sequelize} = require("./bd");
-
+app.use("/public" , express.static('public'));
 app.use(express.json());
 app.use(cors());
-
+app.use(fileupload());
 
 app.get('/', (req, res) => {
     res.send({mensagem: "bem vindo"});
--- rotas/produtos.js
+++ rotas/produtos.js
@@ -1,4 +1,5 @@
 const {Router} = require("express");
+const path = require("path");
 const {criar, listar, remover, buscarPorId, atualizar} = require("../controller/produtos");
 const router = Router();
 
@@ -73,6 +74,33 @@ router.delete("/:id", async (req, res) => {
     
     
 });
+
+
+// router.post("/upload", async (req, res) => {
+//     try {
+//       const { arquivo } = req.files;
+  
+//       let diretorio = path.join(__dirname, "../public");
+  
+//       let nomeArquivo = arquivo.md5;
+//       let extensao = arquivo.mimetype.split("/")[1];
+  
+//       arquivo.mv(`${diretorio}/${nomeArquivo}.${extensao}`, (erro) => {
+//         if (erro) {
+//           throw erro;
+//         }
+  
+//         res.json({
+//           arquivo: `public/${nomeArquivo}.${extensao}`,
+//           nomeArquivo: arquivo.name,
+//         });
+//       });
+//     } catch (erro) {
+//       console.log(erro);
+//       res.status(500).send({ erro });
+//     }
+//   });
+
 
 
 module.exports = router;
